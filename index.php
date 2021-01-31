cf634b626339e775c833bba330215c4f470f7d70
Tamoghna Kommaraju <tamoghnak13@gmail.com>
2021-01-31 11:44:43 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,128 @@
+<html>
+<head>
+<link crossorigin="anonymous" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.6/css/bootstrap.css" integrity="sha256-fDZ6wHbmob72H9d6oq3P6/F3Hc+wHBjdi6FTsUvvQjs=" rel="stylesheet">
+<link crossorigin="anonymous" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha256-eZrrJcwDc/3uDhsdt61sL2oOBY362qM3lon1gyExkL0=" rel="stylesheet">
+<link href="http://www.volokit.com/wp-content/themes/Newspaper/volo-schedule.css" rel="stylesheet">  
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async="" src="https://www.googletagmanager.com/gtag/js?id=UA-144272804-1" type="b1cbd2a77f1540e54ec092ce-text/javascript"></script>
+<script type="b1cbd2a77f1540e54ec092ce-text/javascript">
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-144272804-1');
+</script>
+</head>
+<body>
+<div id="main"><div id="events"><table class="table"><tbody id="pagedContent">
+<tr class="date"><td colspan="4"><img alt="NHL Streams" height="44" src="https://a.espncdn.com/combiner/i?img=/i/teamlogos/leagues/500/nhl.png" width="44" /> Sunday, Jan 31, 2021 <img alt="NHL Streams" height="44" src="https://a.espncdn.com/combiner/i?img=/i/teamlogos/leagues/500/nhl.png" width="44" /></td></tr>
+<tr class="vevent theevent" id="game-bg">   <td class="time" itemscope="" itemtype="http://schema.org/BroadcastEvent">
+<span class="timegmt dtstart" title="Time of the game">1:00 PM ET</span>
+</td>   <td class="teamtd event">
+<span class="team teamleft">
+<img alt="NHL Streams" height="44" src="https://a.espncdn.com/combiner/i?img=/i/teamlogos/nhl/500/scoreboard/nj.png" width="44" />
+</span>
+<span class="at">@</span>
+<span class="team">
+<img alt="NHL Streams" height="44" src="https://a.espncdn.com/combiner/i?img=/i/teamlogos/nhl/500/scoreboard/buf.png" width="44" />
+</span>
+<a class="url hidden-xs-down summary" href="http://www.volokit.com/volostream/nhl-games/new-jersey-buffalo/" target="_blank">New Jersey @ Buffalo</a>
+</td>   <td class="play" style="white-space: nowrap;"><a class="btn btn-gray" href="http://www.volokit.com/volostream/nhl-games/new-jersey-buffalo/" target="_blank">WATCH</a>
+</td></tr>
+<tr class="vevent theevent">   <td class="time" itemscope="" itemtype="http://schema.org/BroadcastEvent">
+<span class="timegmt dtstart" title="Time of the game">5:00 PM ET</span>
+</td>   <td class="teamtd event">
+<span class="team teamleft">
+<img alt="NHL Streams" height="44" src="https://a.espncdn.com/combiner/i?img=/i/teamlogos/nhl/500/scoreboard/dal.png" width="44" />
+</span>
+<span class="at">@</span>
+<span class="team">
+<img alt="NHL Streams" height="44" src="https://a.espncdn.com/combiner/i?img=/i/teamlogos/nhl/500/scoreboard/car.png" width="44" />
+</span>
+<a class="url hidden-xs-down summary" href="http://www.volokit.com/volostream/nhl-games/dallas-carolina/" target="_blank">Dallas @ Carolina</a>
+</td>   <td class="play" style="white-space: nowrap;"><a class="btn btn-gray" href="http://www.volokit.com/volostream/nhl-games/dallas-carolina/" target="_blank">WATCH</a>
+</td></tr>
+  
+  <tr class="vevent theevent" id="game-bg">   <td class="time" itemscope="" itemtype="http://schema.org/BroadcastEvent">
+<span class="timegmt dtstart" title="Time of the game">5:00 PM ET</span>
+</td>   <td class="teamtd event">
+<span class="team teamleft">
+<img alt="NHL Streams" height="44" src="https://a.espncdn.com/combiner/i?img=/i/teamlogos/nhl/500/scoreboard/fla.png" width="44" />
+</span>
+<span class="at">@</span>
+<span class="team">
+<img alt="NHL Streams" height="44" src="https://a.espncdn.com/combiner/i?img=/i/teamlogos/nhl/500/scoreboard/det.png" width="44" />
+</span>
+<a class="url hidden-xs-down summary" href="http://www.volokit.com/volostream/nhl-games/florida-detroit/" target="_blank">Florida @ Detroit</a>
+</td>   <td class="play" style="white-space: nowrap;"><a class="btn btn-gray" href="http://www.volokit.com/volostream/nhl-games/florida-detroit/" target="_blank">WATCH</a>
+</td></tr>
+<tr class="vevent theevent">   <td class="time" itemscope="" itemtype="http://schema.org/BroadcastEvent">
+<span class="timegmt dtstart" title="Time of the game">7:00 PM ET</span>
+</td>   <td class="teamtd event">
+<span class="team teamleft">
+<img alt="NHL Streams" height="44" src="https://a.espncdn.com/combiner/i?img=/i/teamlogos/nhl/500/scoreboard/cbj.png" width="44" />
+</span>
+<span class="at">@</span>
+<span class="team">
+<img alt="NHL Streams" height="44" src="https://a.espncdn.com/combiner/i?img=/i/teamlogos/nhl/500/scoreboard/chi.png" width="44" />
+</span>
+<a class="url hidden-xs-down summary" href="http://www.volokit.com/volostream/nhl-games/columbus-chicago/" target="_blank">Columbus @ Chicago</a>
+</td>   <td class="play" style="white-space: nowrap;"><a class="btn btn-gray" href="http://www.volokit.com/volostream/nhl-games/columbus-chicago/" target="_blank">WATCH</a>
+</td></tr>
+  
+  <tr class="vevent theevent" id="game-bg">   <td class="time" itemscope="" itemtype="http://schema.org/BroadcastEvent">
+<span class="timegmt dtstart" title="Time of the game">7:00 PM ET</span>
+</td>   <td class="teamtd event">
+<span class="team teamleft">
+<img alt="NHL Streams" height="44" src="https://a.espncdn.com/combiner/i?img=/i/teamlogos/nhl/500/scoreboard/nyi.png" width="44" />
+</span>
+<span class="at">@</span>
+<span class="team">
+<img alt="NHL Streams" height="44" src="https://a.espncdn.com/combiner/i?img=/i/teamlogos/nhl/500/scoreboard/phi.png" width="44" />
+</span>
+<a class="url hidden-xs-down summary" href="http://www.volokit.com/volostream/nhl-games/ny-islanders-philadelphia/" target="_blank">NY Islanders @ Philadelphia</a>
+</td>   <td class="play" style="white-space: nowrap;"><a class="btn btn-gray" href="http://www.volokit.com/volostream/nhl-games/ny-islanders-philadelphia/" target="_blank">WATCH</a>
+</td></tr>
+<tr class="vevent theevent">   <td class="time" itemscope="" itemtype="http://schema.org/BroadcastEvent">
+<span class="timegmt dtstart" title="Time of the game">8:00 PM ET</span>
+</td>   <td class="teamtd event">
+<span class="team teamleft">
+<img alt="NHL Streams" height="44" src="https://a.espncdn.com/combiner/i?img=/i/teamlogos/nhl/500/scoreboard/stl.png" width="44" />
+</span>
+<span class="at">@</span>
+<span class="team">
+<img alt="NHL Streams" height="44" src="https://a.espncdn.com/combiner/i?img=/i/teamlogos/nhl/500/scoreboard/ana.png" width="44" />
+</span>
+<a class="url hidden-xs-down summary" href="http://www.volokit.com/volostream/nhl-games/st-louis-anaheim/" target="_blank">St. Louis @ Anaheim</a>
+</td>   <td class="play" style="white-space: nowrap;"><a class="btn btn-gray" href="http://www.volokit.com/volostream/nhl-games/st-louis-anaheim/" target="_blank">WATCH</a>
+</td></tr>
+  
+  <tr class="vevent theevent" id="game-bg">   <td class="time" itemscope="" itemtype="http://schema.org/BroadcastEvent">
+<span class="timegmt dtstart" title="Time of the game">8:00 PM ET</span>
+</td>   <td class="teamtd event">
+<span class="team teamleft">
+<img alt="NHL Streams" height="44" src="https://a.espncdn.com/combiner/i?img=/i/teamlogos/nhl/500/scoreboard/col.png" width="44" />
+</span>
+<span class="at">@</span>
+<span class="team">
+<img alt="NHL Streams" height="44" src="https://a.espncdn.com/combiner/i?img=/i/teamlogos/nhl/500/scoreboard/min.png" width="44" />
+</span>
+<a class="url hidden-xs-down summary" href="http://www.volokit.com/volostream/nhl-games/colorado-minnesota/" target="_blank">Colorado @ Minnesota</a>
+</td>   <td class="play" style="white-space: nowrap;"><a class="btn btn-gray" href="http://www.volokit.com/volostream/nhl-games/colorado-minnesota/" target="_blank">WATCH</a>
+</td></tr>
+<tr class="vevent theevent">   <td class="time" itemscope="" itemtype="http://schema.org/BroadcastEvent">
+<span class="timegmt dtstart" title="Time of the game">9:00 PM ET</span>
+</td>   <td class="teamtd event">
+<span class="team teamleft">
+<img alt="NHL Streams" height="44" src="https://a.espncdn.com/combiner/i?img=/i/teamlogos/nhl/500/scoreboard/ott.png" width="44" />
+</span>
+<span class="at">@</span>
+<span class="team">
+<img alt="NHL Streams" height="44" src="https://a.espncdn.com/combiner/i?img=/i/teamlogos/nhl/500/scoreboard/edm.png" width="44" />
+</span>
+<a class="url hidden-xs-down summary" href="http://www.volokit.com/volostream/nhl-games/ottawa-edmonton/" target="_blank">Ottawa @ Edmonton</a>
+</td>   <td class="play" style="white-space: nowrap;"><a class="btn btn-gray" href="http://www.volokit.com/volostream/nhl-games/ottawa-edmonton/" target="_blank">WATCH</a>
+</td></tr>
+  
+</tbody> </table></div></div><script src="https://ajax.cloudflare.com/cdn-cgi/scripts/7089c43e/cloudflare-static/rocket-loader.min.js" data-cf-settings="b1cbd2a77f1540e54ec092ce-|49" defer=""></script></body>
+</html>
